d8eb0f1fd01419c3f0daefce0004647e2e9b1596
prashant-m4 <prashantsingh090798@gmail.com>
2025-08-24 21:00:18 +0530

    feat: added LSP code

---
+++ SOLID/Liskov/Problem/Main.java
@@ -0,0 +1,35 @@
+package SOLID.Liskov.Problem;
+
+
+// Problems with heavy inheritance : 
+// Tight coupling – Child classes depend too much on parent logic. Any change in the parent might break many children.
+class Bird {
+    public void fly() {
+        System.out.println("Bird is flying");
+    }
+}
+
+class Sparrow extends Bird {
+    @Override
+    public void fly() {
+        System.out.println("Sparrow is flying fast");
+    }
+}
+
+// Problem: Penguin is a Bird, but it can't fly
+class Penguin extends Bird {
+    @Override
+    public void fly() {
+        throw new UnsupportedOperationException("Penguins can't fly!");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Bird bird1 = new Sparrow();
+        bird1.fly(); // ✅ works fine
+
+        Bird bird2 = new Penguin();
+        bird2.fly(); // ❌ runtime error → violates LSP
+    }
+}
---
+++ SOLID/Liskov/Solution/Main.java
@@ -0,0 +1,43 @@
+package SOLID.Liskov.Solution;
+
+// Common Bird interface
+interface Bird {
+    void eat();
+}
+
+interface Flyable {
+    void fly();
+}
+
+class Sparrow implements Bird, Flyable {
+    @Override
+    public void eat() {
+        System.out.println("Sparrow is eating");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("Sparrow is flying");
+    }
+}
+
+class Penguin implements Bird {
+    @Override
+    public void eat() {
+        System.out.println("Penguin is eating");
+    }
+    // No fly() here → no violation
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Bird sparrow = new Sparrow();
+        Bird penguin = new Penguin();
+
+        sparrow.eat();
+        penguin.eat();
+
+        Flyable flyingBird = new Sparrow(); // Works fine
+        flyingBird.fly(); // ✅ LSP holds
+    }
+}
